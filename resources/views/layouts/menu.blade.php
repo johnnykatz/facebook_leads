0dbf912ead0ef13573a99d2f2ed6e512f152660c
johnnykatz <johnnykatzg@gmail.com>
2017-05-23 14:22:11 -0300

    se modifica menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -11,25 +11,25 @@
 <li class="{{ Request::is('admin/users*') ? 'active' : '' }}">
     <a href="{!! route('admin.users.index') !!}"><i class="fa fa-users"></i><span>Usuarios</span></a>
 </li>
-<li class="{{ Request::is('crms*') ? 'active' : '' }}">
-    <a href="{!! route('admin.crms.index') !!}"><i class="fa fa-edit"></i><span>Crms</span></a>
-</li>
+{{--<li class="{{ Request::is('crms*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.crms.index') !!}"><i class="fa fa-edit"></i><span>Crms</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('servicioCrms*') ? 'active' : '' }}">
-    <a href="{!! route('admin.servicioCrms.index') !!}"><i class="fa fa-edit"></i><span>ServicioCrms</span></a>
-</li>
+{{--<li class="{{ Request::is('servicioCrms*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.servicioCrms.index') !!}"><i class="fa fa-edit"></i><span>ServicioCrms</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('campoServicioCrms*') ? 'active' : '' }}">
-    <a href="{!! route('admin.campoServicioCrms.index') !!}"><i class="fa fa-edit"></i><span>CampoServicioCrms</span></a>
-</li>
+{{--<li class="{{ Request::is('campoServicioCrms*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.campoServicioCrms.index') !!}"><i class="fa fa-edit"></i><span>CampoServicioCrms</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('servicioCrmXFormularios*') ? 'active' : '' }}">
-    <a href="{!! route('admin.servicioCrmXFormularios.index') !!}"><i class="fa fa-edit"></i><span>ServicioCrmXFormularios</span></a>
-</li>
+{{--<li class="{{ Request::is('servicioCrmXFormularios*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.servicioCrmXFormularios.index') !!}"><i class="fa fa-edit"></i><span>ServicioCrmXFormularios</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('asociacionCamposServicios*') ? 'active' : '' }}">
-    <a href="{!! route('admin.asociacionCamposServicios.index') !!}"><i class="fa fa-edit"></i><span>AsociacionCamposServicios</span></a>
-</li>
+{{--<li class="{{ Request::is('asociacionCamposServicios*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.asociacionCamposServicios.index') !!}"><i class="fa fa-edit"></i><span>AsociacionCamposServicios</span></a>--}}
+{{--</li>--}}
 
 {{--<li class="{{ Request::is('formularioEnviadoXservicios*') ? 'active' : '' }}">--}}
     {{--<a href="{!! route('admin.formularioEnviadoXservicios.index') !!}"><i class="fa fa-edit"></i><span>formularioEnviadoXservicios</span></a>--}}

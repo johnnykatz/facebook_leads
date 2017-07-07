bd35062c38f9c2b0777a23649a24c8af746212a4
johnnykatz <johnnykatzg@gmail.com>
2017-07-06 21:06:12 -0300

    fix menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -8,9 +8,9 @@
     <a href="{!! route('admin.formularios.index') !!}"><i class="fa fa-edit"></i><span>Formularios</span></a>
 </li>
 
-<li class="{{ Request::is('admin/users*') ? 'active' : '' }}">
-    <a href="{!! route('admin.users.index') !!}"><i class="fa fa-users"></i><span>Usuarios</span></a>
-</li>
+{{--<li class="{{ Request::is('admin/users*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('admin.users.index') !!}"><i class="fa fa-users"></i><span>Usuarios</span></a>--}}
+{{--</li>--}}
 {{--<li class="{{ Request::is('crms*') ? 'active' : '' }}">--}}
     {{--<a href="{!! route('admin.crms.index') !!}"><i class="fa fa-edit"></i><span>Crms</span></a>--}}
 {{--</li>--}}
